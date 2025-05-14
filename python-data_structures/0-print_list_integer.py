f2c20545c6dd66d5608fd1b32a6a5fde648b5e43
babakyusifov <babek@div.edu.az>
2025-05-14 20:42:19 +0400

    fix

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for number in my_list:  # my_list-dəki hər bir element üçün dövr et
-        print("{:d}".format(number))  # hər bir elementi formatla çap et
+    for number in my_list:
+        print("{:d}".format(number))
