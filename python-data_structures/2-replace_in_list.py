95c1effb33ea0fbe763dbece51be54370aa91950
babak.yusifov <babek@div.edu.az>
2025-05-17 18:20:24 +0400

    s

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if 0 <= idx <  len(my_list):
-            my_list[idx] = element
-            return(my_list)
-    
+replace_in_list = __import__('2-replace_in_list').replace_in_list
+
 my_list = [1, 2, 3, 4, 5]
 idx = 3
 new_element = 9
