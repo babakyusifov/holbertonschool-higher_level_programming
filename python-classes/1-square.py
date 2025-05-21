aba7bea8fe50666b15bd8dcd2df232d62c23a6ab
babakyusifov <babek@div.edu.az>
2025-05-21 20:51:54 +0400

    edited

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 '''
-this module 1-square right now it is empty
+This module 1-square right now it is empty
 '''
 
 
 class Square:
     '''
-    squre class is empty
+    Square class is empty
     '''
     def __init__(self, size):
         self.__size = size
-        
