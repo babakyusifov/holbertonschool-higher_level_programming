5c7381e26239d3b6891cb7d022a32c0b95b96b01
babakyusifov <babek@div.edu.az>
2025-05-25 23:28:32 +0400

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,8 @@
+#!/usr/bin/python3
 def text_indentation(text):
     """Prints text with 2 new lines after each '.', '?', and ':'."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     new_line = ""
     for char in text:
         new_line += char
@@ -10,7 +10,6 @@ def text_indentation(text):
             print(new_line.strip())
             print()
             new_line = ""
-
     if new_line.strip():
         print(new_line.strip())
 
