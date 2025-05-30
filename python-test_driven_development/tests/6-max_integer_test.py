c58c1d0281d4d5bbb03f715bf0ed6a4c79812e93
Babak <143651892+babakyusifov@users.noreply.github.com>
2025-05-30 17:54:36 +0000

    fix bug

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])"""
-import unittest
+'''
+Module to test max_integer() function
+'''
+
 max_integer = __import__('6-max_integer').max_integer
+import unittest
+
 
 class TestMaxInteger(unittest.TestCase):
-    def test_ordered_list(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-
-    def test_unordered_list(self):
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-
-    def test_max_at_beginning(self):
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
-
-    def test_one_element(self):
-        self.assertEqual(max_integer([42]), 42)
-
-    def test_empty_list(self):
-        self.assertIsNone(max_integer([]))
-
-    def test_negative_numbers(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-
-    def test_mixed_numbers(self):
-        self.assertEqual(max_integer([-10, 5, 3, -1, 0]), 5)
-
+    '''
+    Class for testing max_integer() function with two methods
+    '''
+    def test_max(self):
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1]), 1)
+        self.assertAlmostEqual(max_integer([1,2,3,4,5]), 5)
+        self.assertAlmostEqual(max_integer([10,2,24,52,1]), 52)
+        self.assertAlmostEqual(max_integer([-1,-2,-3,-4,-5]), -1)
+        self.assertAlmostEqual(max_integer([-1,-2,-3,-4, 0]), 0)
+        self.assertAlmostEqual(max_integer([1,2,3,-4,-5]), 3)
