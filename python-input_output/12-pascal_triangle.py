e286d28ab3def87150497053f58e2c22967a848c
babakyusifov <babek@div.edu.az>
2025-06-09 18:02:33 +0400

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,21 +1,18 @@
-#!/usr/bin/python3
-"""
-Pascal's Triangle Module
-"""
-
-
 def pascal_triangle(n):
-    """calculate pascal
-    args:
-        n: value
-    return
-        list
-    """
-    my_list = []
+    """Calculate Pascal's Triangle up to n rows."""
+    triangle = []
     if n <= 0:
-        return my_list
+        return triangle
+
     for i in range(n):
-        num = 11**i
-        li = [int(n) for n in str(num)]
-        my_list.append(li)
-    return my_list
+        if i == 0:
+            triangle.append([1])
+        else:
+            prev_row = triangle[-1]
+            row = [1]  # start with 1
+            # sum of adjacent elements in the previous row
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)  # end with 1
+            triangle.append(row)
+    return triangle
